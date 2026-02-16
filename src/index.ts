17e36838e844ed637bc8427f8cba90fed6631ddf
Jerred Shepherd <github@sjer.red>
2026-02-15 17:18:47 -0800

    chore: moved to monorepo/packages/astro-opengraph-images

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import { astroOpenGraphImages } from "./integration.js";
-
-export * from "./presets/index.js";
-export * from "./types.js";
-export { getImagePath } from "./util.js";
-
-export default astroOpenGraphImages;
